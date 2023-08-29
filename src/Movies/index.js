2121c2632af840dee3956ace02d826b207a002e4
Trex0319 <weichung22y@forwardschool.edu.my>
2023-08-29 15:40:10 +0800

    update

--- src/Movies/index.js
+++ src/Movies/index.js
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Title, Grid, Card, Badge, Group, Space } from "@mantine/core";
-
+import {
+  Title,
+  Grid,
+  Card,
+  Badge,
+  Group,
+  Space,
+  Divider,
+  Button,
+} from "@mantine/core";
 function Movies() {
   const [movies, setMovies] = useState([]);
-
   useEffect(() => {
     axios
       .get("http://localhost:2000/movies")
@@ -15,24 +22,75 @@ function Movies() {
         console.log(error);
       });
   }, []);
-
+  const filterMovie = async (genre = "") => {
+    try {
+      const response = await axios.get(
+        "http://localhost:2000/movies?genre=" + genre
+      );
+      setMovies(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
+      <Space h="20px" />
       <Title order={3} align="center">
         Movies
       </Title>
+      <Space h="20px" />
+      <Group position="center">
+        <Button
+          onClick={() => {
+            filterMovie("");
+          }}
+        >
+          All
+        </Button>
+        <Button
+          onClick={() => {
+            filterMovie("Drama");
+          }}
+        >
+          Drama
+        </Button>
+        <Button
+          onClick={() => {
+            filterMovie("Fantasy");
+          }}
+        >
+          Fantasy
+        </Button>
+        <Button
+          onClick={() => {
+            filterMovie("Action");
+          }}
+        >
+          Action
+        </Button>
+        <Button
+          onClick={() => {
+            filterMovie("Sci-Fi");
+          }}
+        >
+          Sci-fi
+        </Button>
+      </Group>
+      <Space h="20px" />
       <Grid>
         {movies
           ? movies.map((movie) => {
               return (
-                <Grid.Col key={movie.id} span={4}>
-                  <Card withBorder shadow="sm" p="20px">
+                <Grid.Col span={4} key={movie._id}>
+                  <Card align="center" withBorder shadow="md" p="20px">
                     <Title order={5}>{movie.title}</Title>
-                    <Space h="20px" />
-                    <Group position="center">
+                    <Space h="10px" />
+                    <Divider />
+                    <Space h="10px" />
+                    <Group position="center" spacing={2}>
                       <Badge color="green">{movie.director}</Badge>
-                      <Badge color="yellow">{movie.genre}</Badge>
-                      <Badge color="grape">{movie.rating}</Badge>
+                      <Badge color="red">{movie.genre}</Badge>
+                      <Badge>{movie.rating}</Badge>
                     </Group>
                   </Card>
                 </Grid.Col>
@@ -43,5 +101,4 @@ function Movies() {
     </>
   );
 }
-
 export default Movies;
--- src/Tvshows/index.js
+++ src/Tvshows/index.js
@@ -1,42 +1,163 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { Title, Grid, Card, Badge, Group, Space } from "@mantine/core";
-
-function Tvshows() {
-  const [tvshows, setTvshows] = useState([]);
-
+import {
+  Title,
+  Grid,
+  Card,
+  Badge,
+  Group,
+  Space,
+  Divider,
+  Button,
+} from "@mantine/core";
+function Tvshow() {
+  const [tvshow, setTvshow] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:2000/tvshows")
       .then((response) => {
-        setTvshows(response.data);
+        setTvshow(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   }, []);
-
+  const filterTvshow = async (genre = "") => {
+    try {
+      const response = await axios.get(
+        "http://localhost:2000/tvshows?genre=" + genre
+      );
+      setTvshow(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
+      <Space h="20px" />
       <Title order={3} align="center">
-        Tvshows
+        TV Shows
       </Title>
+      <Space h="20px" />
+      <Group position="center">
+        <Button
+          onClick={() => {
+            filterTvshow("");
+          }}
+        >
+          All
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Drama");
+          }}
+        >
+          Drama
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Fantasy");
+          }}
+        >
+          Fantasy
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Action");
+          }}
+        >
+          Action
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Crime");
+          }}
+        >
+          Crime
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Crime");
+          }}
+        >
+          Crime
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Adventure");
+          }}
+        >
+          Adventure
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Adventure");
+          }}
+        >
+          Adventure
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Biography");
+          }}
+        >
+          Biography
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Horror");
+          }}
+        >
+          Horror
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Sci-Fi");
+          }}
+        >
+          Sci-Fi
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Comedy");
+          }}
+        >
+          Comedy
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Mystery");
+          }}
+        >
+          Mystery
+        </Button>
+        <Button
+          onClick={() => {
+            filterTvshow("Thriller");
+          }}
+        >
+          Thriller
+        </Button>
+      </Group>
+      <Space h="20px" />
       <Grid>
-        {tvshows
-          ? tvshows.map((tvshow) => {
+        {tvshow
+          ? tvshow.map((tvshows) => {
               return (
-                <Grid.Col key={tvshow.id} span={4}>
-                  <Card withBorder shadow="sm" p="20px">
-                    <Title order={5}>{tvshow.title}</Title>
-                    <Space h="20px" />
-                    <Group position="center">
-                      <Badge color="green">{tvshow.creator}</Badge>
-                      {tvshow.genre.map((genre) => (
-                        <Badge color="yellow" key={genre}>
+                <Grid.Col span={4} key={tvshows._id}>
+                  <Card align="center" withBorder shadow="md" p="20px">
+                    <Title order={5}>{tvshows.title}</Title>
+                    <Space h="10px" />
+                    <Divider />
+                    <Space h="10px" />
+                    <Group position="center" spacing={2}>
+                      <Badge color="green">{tvshows.creator}</Badge>
+                      {tvshows.genre.map((genre) => (
+                        <Badge color="red" key={tvshows.genre}>
                           {genre}
                         </Badge>
                       ))}
-                      <Badge color="grape">{tvshow.rating}</Badge>
+                      <Badge>{tvshows.rating}</Badge>
                     </Group>
                   </Card>
                 </Grid.Col>
@@ -47,5 +168,4 @@ function Tvshows() {
     </>
   );
 }
-
-export default Tvshows;
+export default Tvshow;
